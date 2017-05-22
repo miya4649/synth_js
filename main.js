6745d28e2978044040b7481dbb555b01e4c00f97
miya <miya4649@users.noreply.github.com>
2017-05-22 23:21:04 +0900

    new tracks

---
+++ main.js
@@ -0,0 +1,153 @@
+/*
+  Copyright (c) 2017, miya
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var FIXED_BITS = 14;
+var FIXED_BITS_ENV = 8;
+var FIXED_SCALE = (1 << FIXED_BITS);
+var ENV_VALUE_MAX = (1 << FIXED_BITS << FIXED_BITS_ENV);
+
+var seq = new Sequencer();
+
+var stop = function()
+{
+  seq.stop();
+};
+
+var playMonologue = function()
+{
+  var i;
+  var OSCS = 4;
+  var PART_VOLUME = (Math.floor(FIXED_SCALE / OSCS));
+  var MOD_LEVEL_MAX = (Math.floor(FIXED_SCALE * 0.52));
+  var REVERB_VOLUME = (Math.floor(FIXED_SCALE * 0.3));
+  var OUT_VOLUME = (1.0 / FIXED_SCALE);
+  var chordData = [
+    [0,2,4,7,11,0,0,0],
+    [2,4,5,7,11,0,0,0],
+    [0,4,7,9,11,0,0,0],
+    [0,4,5,7,9,0,0,0],
+    [0,2,4,5,9,0,0,0],
+    [2,5,7,9,11,0,0,0]
+  ];
+  var progressionData = [
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 3]
+  ];
+  var bassData = [0,1,3,2,1,2];
+  stop();
+  seq.setSeqParam(6.0, 16, 4, 5, 3, 16, 4, true, false);
+  seq.setChordData(5, chordData, progressionData, bassData);
+  seq.synth.setSynthParam(OSCS, 0.278639455782, 0.136533333333, REVERB_VOLUME, 0.5, OUT_VOLUME);
+  seq.init();
+  for (i = 0; i < OSCS; i++)
+  {
+    seq.synth.getParams(i).envelopeDiffA = ENV_VALUE_MAX >> 3;
+    seq.synth.getParams(i).envelopeDiffD = (- ENV_VALUE_MAX) >> 15;
+    seq.synth.getParams(i).envelopeDiffR = (- ENV_VALUE_MAX) >> 13;
+    seq.synth.getParams(i).modLevel0 = MOD_LEVEL_MAX;
+  }
+  seq.synth.getParams(0).levelL = PART_VOLUME >> 1;
+  seq.synth.getParams(0).levelR = PART_VOLUME;
+  seq.synth.getParams(1).levelL = PART_VOLUME;
+  seq.synth.getParams(1).levelR = PART_VOLUME >> 1;
+  seq.start();
+};
+
+var playLuna = function()
+{
+  var i;
+  var OSCS = 4;
+  var PART_VOLUME = (Math.floor(FIXED_SCALE / OSCS));
+  var MOD_LEVEL_MAX = (Math.floor(FIXED_SCALE * 0.52));
+  var REVERB_VOLUME = (Math.floor(FIXED_SCALE * 0.3));
+  var OUT_VOLUME = (1.0 / FIXED_SCALE);
+  var chordData = [
+    [0,2,4,5,7,0,0,0],
+    [0,2,5,7,9,0,0,0],
+    [0,2,5,7,11,0,0,0]
+  ];
+  var progressionData = [
+    [0, 3],
+    [0, 3],
+    [0, 1]
+  ];
+  var bassData = [0,2,3];
+  stop();
+  seq.setSeqParam(1.0, 8, 8, 6, 3, 16, 4, false, true);
+  seq.setChordData(5, chordData, progressionData, bassData);
+  seq.synth.setSynthParam(OSCS, 0.557278911565, 0.519439673469, REVERB_VOLUME, 0.8, OUT_VOLUME);
+  seq.init();
+  for (i = 0; i < OSCS; i++)
+  {
+    seq.synth.getParams(i).envelopeDiffA = ENV_VALUE_MAX >> 7;
+    seq.synth.getParams(i).envelopeDiffD = (- ENV_VALUE_MAX) >> 17;
+    seq.synth.getParams(i).envelopeDiffR = (- ENV_VALUE_MAX) >> 15;
+    seq.synth.getParams(i).modLevel0 = MOD_LEVEL_MAX;
+  }
+  seq.synth.getParams(0).levelL = PART_VOLUME >> 1;
+  seq.synth.getParams(0).levelR = PART_VOLUME;
+  seq.synth.getParams(1).levelL = PART_VOLUME;
+  seq.synth.getParams(1).levelR = PART_VOLUME >> 1;
+  seq.start();
+};
+
+var playIntoxication = function()
+{
+  var i;
+  var OSCS = 4;
+  var PART_VOLUME = (Math.floor(FIXED_SCALE / OSCS));
+  var MOD_LEVEL_MAX = (Math.floor(FIXED_SCALE * 0.52));
+  var REVERB_VOLUME = (Math.floor(FIXED_SCALE * 0.3));
+  var OUT_VOLUME = (1.0 / FIXED_SCALE);
+  var chordData = [
+    [0,4,7,0,0,0,0,0],
+    [4,7,11,0,0,0,0,0],
+    [0,4,9,0,0,0,0,0],
+    [0,5,9,0,0,0,0,0],
+    [2,5,9,0,0,0,0,0],
+    [2,7,11,0,0,0,0,0]
+  ];
+  var progressionData = [
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 3]
+  ];
+  var bassData = [0,0,2,1,0,1];
+  stop();
+  seq.setSeqParam(5.0, 6, 3, 5, 3, 16, 8, true, true);
+  seq.setChordData(3, chordData, progressionData, bassData);
+  seq.synth.setSynthParam(OSCS, 0.4, 0.8123, REVERB_VOLUME, 0.8, OUT_VOLUME);
+  seq.init();
+  for (i = 0; i < OSCS; i++)
+  {
+    seq.synth.getParams(i).envelopeDiffA = ENV_VALUE_MAX >> 3;
+    seq.synth.getParams(i).envelopeDiffD = (- ENV_VALUE_MAX) >> 15;
+    seq.synth.getParams(i).envelopeDiffR = (- ENV_VALUE_MAX) >> 13;
+    seq.synth.getParams(i).modLevel0 = MOD_LEVEL_MAX * rand(6);
+  }
+  seq.synth.getParams(0).levelL = PART_VOLUME >> 1;
+  seq.synth.getParams(0).levelR = PART_VOLUME;
+  seq.synth.getParams(1).levelL = PART_VOLUME;
+  seq.synth.getParams(1).levelR = PART_VOLUME >> 1;
+  seq.start();
+};
+
---
+++ sequencer.js
@@ -0,0 +1,187 @@
+/*
+  Copyright (c) 2017, miya
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+function Sequencer()
+{
+  var self = this;
+  var tempo = 5.0;
+  var seqLength = 8;
+  var bassFrequency = 2;
+  var scaleBufferSize = 16;
+  var seqCounter = 0;
+  var barCounter = 0;
+  var deleteCounter = 0;
+  var chord = 0;
+  var note = 0;
+  var octMin = 5;
+  var octRange = 3;
+  var deleteFrequency = 16;
+  var repeat = 4;
+  var toneChange = true;
+  var appendBass = true;
+  var tempoCount = 0;
+  var scaleTable = [];
+  var chordLength = 3;
+  var chordData = [
+    [0,4,7,0,0,0,0,0],
+    [4,7,11,0,0,0,0,0],
+    [0,4,9,0,0,0,0,0],
+    [0,5,9,0,0,0,0,0],
+    [2,5,9,0,0,0,0,0],
+    [2,7,11,0,0,0,0,0]
+  ];
+  var progressionData = [
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 6],
+    [0, 3]
+  ];
+  var bassData = [0,0,2,1,0,1];
+  var seqData = [];
+
+  this.synth = new Synthesizer();
+
+  function NoteData()
+  {
+    this.note = 0;
+    this.oct = 0;
+  }
+
+  this.stop = function()
+  {
+    if (this.synth)
+    {
+      this.synth.stop();
+    }
+  };
+
+  this.init = function()
+  {
+    var i, j;
+    this.synth.init();
+    tempoCount = Math.floor(this.synth.getSampleRate() / tempo);
+
+    for (i = 0; i < scaleBufferSize; i++)
+    {
+      scaleTable[i] = Math.floor(Math.pow(2.0, i / 12.0) * 440.0 * (0x100000000 / this.synth.getSampleRate() / this.synth.getWaveBufferSize()));
+    }
+
+    for (i = 0; i < this.synth.getOscs(); i++)
+    {
+      seqData[i] = [];
+      for (j = 0; j < seqLength; j++)
+      {
+        seqData[i][j] = new NoteData();
+      }
+    }
+  };
+
+  this.start = function()
+  {
+    seqCounter = 0;
+    barCounter = 0;
+    deleteCounter = 0;
+    chord = 0;
+    note = 0;
+
+    this.synth.setCallback(loop);
+    this.synth.setCallbackRate(tempoCount);
+    this.synth.start();
+  };
+
+  // callback
+  var loop = function()
+  {
+    // sequencer
+    var i, ch, beat, n, mixL, mixR, mixRevL, mixRevR, reverbL, reverbR;
+    var oscs = self.synth.getOscs();
+
+    if (seqCounter >= seqLength)
+    {
+      seqCounter = 0;
+      if (barCounter >= repeat)
+      {
+        barCounter = 0;
+        if (toneChange === true)
+        {
+          for (i = 0; i < oscs; i++)
+          {
+            self.synth.getParams(i).envelopeDiffA = self.synth.getEnvValueMax() >> (rand(11) + 2);
+            self.synth.getParams(i).modLevel0 = self.synth.getModLevelMax() * rand(6);
+          }
+        }
+        chord = rand(progressionData[chord][1]) + progressionData[chord][0];
+        deleteCounter = rand(deleteFrequency);
+      }
+      for (i = 0; i < deleteCounter; i++)
+      {
+        seqData[rand(oscs)][rand(seqLength)].oct = 0;
+      }
+      for (i = 0; i < 4; i++)
+      {
+        ch = rand(oscs);
+        beat = rand(seqLength);
+        seqData[ch][beat].note = rand(chordLength);
+        seqData[ch][beat].oct = rand(octRange) + octMin;
+      }
+      if (appendBass === true)
+      {
+        for (i = 0; i < seqLength; i+=bassFrequency)
+        {
+          seqData[0][i].note = bassData[chord];
+          seqData[0][i].oct = octMin;
+        }
+      }
+      barCounter++;
+    }
+    for (i = 0; i < oscs; i++)
+    {
+      if (seqData[i][seqCounter].oct !== 0)
+      {
+        self.synth.getParams(i).noteOn = true;
+        n = chordData[chord][seqData[i][seqCounter].note];
+        self.synth.getParams(i).pitch = scaleTable[n] << seqData[i][seqCounter].oct;
+      }
+      else
+      {
+        self.synth.getParams(i).noteOn = false;
+      }
+    }
+    seqCounter++;
+  };
+
+  this.setSeqParam = function(tempo_arg, seqLength_arg, bassFrequency_arg, octMin_arg, octRange_arg, deleteFrequency_arg, repeat_arg, toneChange_arg, appendBass_arg)
+  {
+    tempo = tempo_arg;
+    seqLength = seqLength_arg;
+    bassFrequency = bassFrequency_arg;
+    octMin = octMin_arg;
+    octRange = octRange_arg;
+    deleteFrequency = deleteFrequency_arg;
+    repeat = repeat_arg;
+    toneChange = toneChange_arg;
+    appendBass = appendBass_arg;
+  };
+
+  this.setChordData = function(chordLength_arg, chordData_arg, progressionData_arg, bassData_arg)
+  {
+    chordLength = chordLength_arg;
+    chordData = chordData_arg;
+    progressionData = progressionData_arg;
+    bassData = bassData_arg;
+  };
+}
